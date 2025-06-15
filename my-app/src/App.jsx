57356c0b7ef44bd37ab8da4c2bbf7f6657d6ea5d
Gang Wang <650313+HarryWang74@users.noreply.github.com>
2025-06-15 13:24:19 +1000

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,19 +2,21 @@ import './App.css'
 // import { MessageProvider } from './useContextDemo/MessageContext'
 // import Demo from './useContextDemo/Demo'
 
-import { MessageProvider } from './useContextWithStoreDemo/context/MessageProvider'
-import Demo from './useContextWithStoreDemo/Demo'
+// import { MessageProvider } from './useContextWithStoreDemo/context/MessageProvider'
+// import Demo from './useContextWithStoreDemo/Demo'
 
 function App() {
   return (
-    // <h1>Hello</h1>
+    <h1>Hello</h1>
 
     // useContext with store demo
-    <MessageProvider>
-      <Demo />
-    </MessageProvider>
+    /* 
+      <MessageProvider>
+        <Demo />
+      </MessageProvider> 
+    */
 
-    // common way to use context
+    // use context
     /*     
       <MessageProvider>
         <Demo />
